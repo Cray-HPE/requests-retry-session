61c28072dfaf5b58360e7706b01f7a13fe1f106a
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2026-04-17 12:48:09 -0400

    test

---
+++ run_test_rrs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# MIT License
+#
+# (C) Copyright 2026 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+
+set -euo pipefail
+
+IMG="pytest-${PY_VERSION}:${DOCKER_VERSION}"
+
+python3 ./gen_test_constraints.py | while read ARGS; do
+    # Deliberately do not quote $ARGS, so it is split into multiple arguments
+    # if there is whitespace in it, or it will not be an argument at all,
+    # if it is empty
+    docker run "$IMG" $ARGS || exit 1
+done
+
+exit 0
