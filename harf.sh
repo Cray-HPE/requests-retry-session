5d55ceaab5eb1772f28ead268dad7c97f892c01f
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2026-05-18 12:04:52 -0400

    rpm macros

--- harf.sh
+++ harf.sh
@@ -24,9 +24,9 @@
 set -x
 
 zypper --non-interactive ar --no-gpgcheck https://download.opensuse.org/tumbleweed/repo/oss/repodata tumbleweed/repo/oss/repodata
-zypper --non-interactive in python-rpm-macros
-zypper --non-interactive in python3-rpm-macros
-zypper --non-interactive in pyproject-rpm-macros
+zypper --non-interactive in -f --no-confirm python-rpm-macros
+zypper --non-interactive in -f --no-confirm python3-rpm-macros
+zypper --non-interactive in -f --no-confirm pyproject-rpm-macros
 
 rpm -a | grep -E 'rpm|macro'
 rpm --eval '%pyproject_save_files foo'
