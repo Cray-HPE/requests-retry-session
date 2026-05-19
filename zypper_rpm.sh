d6cb601c46fd9292c06ef34bdddd1bfb26cac477
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2026-05-19 10:59:42 -0400

    harf.sh

--- zypper_rpm.sh
+++ zypper_rpm.sh
@@ -27,5 +27,6 @@ set -exo pipefail
 
 zypper --non-interactive ar -f https://download.opensuse.org/tumbleweed/repo/oss/ tumbleweed-oss
 zypper --non-interactive --gpg-auto-import-keys refresh
+zypper --non-interactive wp '*/python3.attr' || true
 zypper --non-interactive install -f -y --force-resolution python3-rpm-macros python-rpm-generators rpm rpm-config-SUSE
 find /usr/lib/rpm/fileattrs/ -type f -name python3.attr -print
